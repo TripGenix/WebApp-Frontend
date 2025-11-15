1468d4cc5c62cb15cd011ac6fec4a3cc411df52a
gomes-milindu <gomesmilindu@gmail.com>
2025-11-15 12:19:38 +0530

    change the vite dependency into stable version in package.json file

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,6 +1,6 @@
 function Booking(){
     return(
-        <div className="w-full h-[100vh] flex justify-center items-center">
+        <div className="w-full h-screen flex justify-center items-center">
             <h1 className="text-2xl">This is Booking Page</h1>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 function Home(){
     return(
-        <div className="w-full h-[100vh] flex justify-center items-center">
+        <div className="w-full h-screen flex justify-center items-center">
             <h1 className="text-2xl">This is Home Page</h1>
         </div>
     )
